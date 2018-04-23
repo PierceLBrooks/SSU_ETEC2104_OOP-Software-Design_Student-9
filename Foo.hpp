3d9a3ea7de965654a8513f32d9990425089c8494
Pierce Brooks <piercebrks@gmail.com>
2018-04-23 15:05:14 -0400

    initialization

---
+++ Foo.hpp
@@ -0,0 +1,15 @@
+// Author: Pierce Brooks
+
+#ifndef FOO_HPP
+#define FOO_HPP
+
+class Foo
+{
+    public:
+        Foo(int bar);
+        int getBar() const;
+    private:
+        int bar;
+};
+
+#endif // FOO_HPP
